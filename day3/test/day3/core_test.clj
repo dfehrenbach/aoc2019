19a21ce46d8cd71d520b3918ea8764aec93780e7
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2019-12-05 15:05:51 -0600

    Day3 Part2
    Did an interesting answer that involved not redoing any of the old code. I took the intersections and then traced all the way to each intersection.
    
    Could have also just left a running count as I was building out the lines themselves. Either works, and I think this second one is more effecient and easier to understand. It just takes a heftier refactoring than simply reimaginng what I already have access to.

--- day3/test/day3/core_test.clj
+++ day3/test/day3/core_test.clj
@@ -3,7 +3,8 @@
             [day3.core :refer [create-points
                                build-lines
                                manhatten-distance
-                               main]]))
+                               part1-solution
+                               travelled-distance]]))
 
 (deftest create-points-test
   (testing "Handles up"
@@ -27,16 +28,16 @@
 
 (deftest build-lines-test
   (testing "Handles horizontal lines"
-    (is (= #{[0 0] [1 0] [2 0] [3 0] [4 0] [5 0]}
+    (is (= [[1 0] [2 0] [3 0] [4 0] [5 0]]
            (build-lines [[0 0] [5 0]]))))
   (testing "Handles vertical lines"
-    (is (= #{[0 0] [0 1] [0 2] [0 3] [0 4] [0 5]}
+    (is (= [[0 1] [0 2] [0 3] [0 4] [0 5]]
            (build-lines [[0 0] [0 5]]))))
   (testing "Handles 3 points forming a corner"
-    (is (= #{[0 0] [1 0] [2 0] [2 1] [2 2] [2 3]}
+    (is (= [[1 0] [2 0] [2 1] [2 2] [2 3]]
            (build-lines [[0 0] [2 0] [2 3]]))))
   (testing "Handles 3 points forming a line"
-    (is (= #{[0 0] [1 0] [2 0] [3 0] [4 0] [5 0]}
+    (is (= [[1 0] [2 0] [3 0] [4 0] [5 0]]
            (build-lines [[0 0] [2 0] [5 0]])))))
 
 (deftest manhatten-distance-test
@@ -47,12 +48,16 @@
   (testing "Given a positive and negative together, uses absolute value"
     (is (= 10 (manhatten-distance [-5 5])))))
 
-(deftest main-test
+(deftest part1-solution-test
   (testing "4 Point lines example"
-    (is (= 6 (:distance (main [["R8" "U5" "L5" "D3"] ["U7" "R6" "D4" "L4"]])))))
+    (is (= 6 (:distance (part1-solution [["R8" "U5" "L5" "D3"] ["U7" "R6" "D4" "L4"]])))))
   (testing "4 Point lines example"
-    (is (= 159 (:distance (main [["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"]
-                                 ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"]])))))
+    (is (= 159 (:distance (part1-solution [["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"]
+                                           ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"]])))))
   (testing "4 Point lines example"
-    (is (= 135 (:distance (main [["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"]
-                                 ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"]]))))))
+    (is (= 135 (:distance (part1-solution [["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"]
+                                           ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"]]))))))
+
+(deftest travelled-distance-test
+  (testing "Given a set of crosses and the original lines, give a map of distances travelled for each wire"
+    (is (= [{:l1 10 :l2 10 :cross [5 5]}] (travelled-distance [[5 5]] [["R5" "U5"] ["U5" "R5"]])))))
