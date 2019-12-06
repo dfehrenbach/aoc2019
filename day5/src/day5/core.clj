7d7b7c92c4d3812e0ed298754731a43cb554fa11
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2019-12-06 01:26:56 -0600

    Complete Day 5 part 2

--- day5/src/day5/core.clj
+++ day5/src/day5/core.clj
@@ -5,68 +5,81 @@
                    #(Integer/parseInt %)
                    (str/split (slurp "resources/input.txt") #",|\r\n")))
 
-; DAY 5 Part 1
+(def program-input (atom 5))
+(def final-output (atom 0))
 
-
-(Integer/parseInt "1")
-
-(comment
-  (map #(Integer/parseInt (str %)) "1011"))
-
-(def program-input 1)
-
-(defn day5-update-program [c acc]
-  (let [[opcode _ m1 m2] (map #(Integer/parseInt (str %)) (reverse (str (first c))))
-        [p1 p2 p3] (rest c)]
+(defn day5-update-program [chunk acc program-index]
+  (let [[opcode _ m1 m2] (map #(Integer/parseInt (str %)) (reverse (str (first chunk))))
+        [p1 p2 p3] (rest chunk)]
     (case opcode
       1 (let [p1 (if (pos-int? m1) p1 (get acc p1))
               p2 (if (pos-int? m2) p2 (get acc p2))]
-          (assoc acc p3 (+ p1 p2)))
+          {:prog (assoc acc p3 (+ p1 p2))
+           :prog-index (+ 4 program-index)})
 
       2 (let [v1 (if (pos-int? m1) p1 (get acc p1))
               v2 (if (pos-int? m2) p2 (get acc p2))]
-          (assoc acc p3 (* v1 v2)))
+          {:prog (assoc acc p3 (* v1 v2))
+           :prog-index (+ 4 program-index)})
 
-      3 (assoc acc p1 program-input) ;; Input 1 to start with
+      3 {:prog (assoc acc p1 (deref program-input))
+         :prog-index (+ 2 program-index)} ;; Input 1 to start with
 
       4 (let [v1 (if (pos-int? m1) p1 (get acc p1))]
-          (println v1)
-          acc)
+          (swap! final-output (fn [_] v1))
+          {:prog acc
+           :prog-index (+ 2 program-index)})
+
+      5 (let [v1 (if (pos-int? m1) p1 (get acc p1))
+              v2 (if (pos-int? m2) p2 (get acc p2))]
+          {:prog acc
+           :prog-index (if (not (zero? v1)) v2
+                           (+ 3 program-index))})
+
+      6 (let [v1 (if (pos-int? m1) p1 (get acc p1))
+              v2 (if (pos-int? m2) p2 (get acc p2))]
+          {:prog acc
+           :prog-index (if (zero? v1) v2
+                           (+ 3 program-index))})
+
+      7 (let [v1 (if (pos-int? m1) p1 (get acc p1))
+              v2 (if (pos-int? m2) p2 (get acc p2))]
+          {:prog (if (< v1 v2) (assoc acc p3 1) (assoc acc p3 0))
+           :prog-index (+ 4 program-index)})
+
+      8 (let [v1 (if (pos-int? m1) p1 (get acc p1))
+              v2 (if (pos-int? m2) p2 (get acc p2))]
+          {:prog (if (= v1 v2) (assoc acc p3 1) (assoc acc p3 0))
+           :prog-index (+ 4 program-index)})
 
       (throw (println "ERROR WITH OPCODE:" opcode)))))
-
-(defn extract-next-opcode [program]
-  (Integer/parseInt (str (last (str (first program))))))
 
 (defn day5-build-program [starting-program]
   (loop [acc starting-program
          program-index 0]
     (let [remaining-program (vec (drop program-index acc))
-          instruction-length (if (or (= (extract-next-opcode remaining-program) 3)
-                                     (= (extract-next-opcode remaining-program) 4)) 2 4)
+          next-op-code (Integer/parseInt (str (last (str (first remaining-program)))))
+          instruction-length (case next-op-code
+                               1 4, 2 4
+                               3 2, 4 2
+                               5 3, 6 3
+                               7 4, 8 4
+                               9 0, (throw (println "ERROR FOR NEXT OP CODE: " next-op-code)))
           chunk (take instruction-length remaining-program)]
-
       (if (= 99 (first remaining-program)) acc
-          (recur (day5-update-program chunk acc)
-                 (+ instruction-length program-index))))))
-
-(day5-build-program root-program)
+          (let [{updated-program :prog
+                 next-program-index :prog-index}
+                (day5-update-program chunk acc program-index)]
+            (recur updated-program
+                   next-program-index))))))
 
 (comment
-  (def restprogram (vec (drop 2 root-program)))
-  (def thing (take 4 restprogram))
-
- ;; => (1 225 6 6)
-
-  (get (vec restprogram) 1)
-
-  (day5-update-program thing restprogram)
-
-  (day5-update-program (take 4 (drop 2 root-program)) (drop 2 root-program))
-
-  (let [[opcode _ m1 m2 m3] (reverse (str (first '(3 233))))]
-    [opcode m1 m2 m3])
-
+  (swap! program-input (fn [_] 1))
   (day5-build-program root-program)
+  (deref final-output)
+;; => 72598358
 
-  (reduce * (repeat 14 0.5)))
+  (swap! program-input (fn [_] 5))
+  (day5-build-program root-program)
+  (deref final-output))
+;; => 11826654
