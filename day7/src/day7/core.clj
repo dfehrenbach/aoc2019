3bae5b8c853bc7776c23ebcacb4c176c965b5478
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2019-12-07 19:07:50 -0600

    Finish part 2... and cri
    OKAY... THIS CODE BREAKS... A LOT
    
    It may be worth rethinking this if this intcode computer comes up again.

--- day7/src/day7/core.clj
+++ day7/src/day7/core.clj
@@ -23,13 +23,13 @@
            :prog-index (+ 4 program-index)})
 
       3 (let [input (<!! inchan)]
-          (println "used input: " input)
           {:prog (assoc acc p1 input)
            :prog-index (+ 2 program-index)}) ;; Input 1 to start with
 
       4 (let [v1 (if (pos-int? m1) p1 (get acc p1))]
           (reset! !final-out v1)
-          (>!! outchan v1)
+          (async/go (>! outchan v1))
+          #_(async/put! outchan v1)
           {:prog acc
            :prog-index (+ 2 program-index)})
 
@@ -70,65 +70,19 @@
                                9 0, (throw (println "ERROR FOR NEXT OP CODE: " next-op-code)))
           chunk (take instruction-length remaining-program)]
       (if (= 99 (first remaining-program))
-        (do
-          (async/close! inchan)
-          (>!! final-chan (deref !final-out)))
+        (async/put! final-chan (deref !final-out))
         (let [{updated-program :prog
                next-program-index :prog-index}
               (day7-update-program chunk acc program-index inchan outchan !final-out)]
           (recur updated-program
                  next-program-index))))))
 
-#_(defn run-programs [program input]
-    (reset! !final-output 0)
-    (reset! !phase-setting true)
-
-    (reset! !program-input input)
-    (dotimes [_ 5] (day7-build-program program))
-    (deref !final-output))
-
-#_(defn try-all-combinations [program]
-    (let [codes (range 5)
-          code-combos (for [a codes
-                            b (remove #{a} codes)
-                            c (remove #{a b} codes)
-                            d (remove #{a b c} codes)
-                            e (remove #{a b c d} codes)]
-                        [a b c d e])]
-      (map (fn [combo]
-             {:combo combo
-              :output (run-programs program combo)}) code-combos)))
-
-#_(defn run-programs-part2 [program input]
-    (reset! !final-output 0)
-    (reset! !phase-setting true)
-    (reset! !first-round true)
-
-    (comment (def input [9 8 7 6 5]))
-    (reset! !program-input input)
-
-    (comment (def program [3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26
-                           27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5]))
-    (dotimes [_ 5] (day7-build-program program))
-
-    (deref !final-output)
-    (reset! !first-round false)
-
-    (repeatedly #(day7-build-program program))
-
-  ;(while (zero? (deref !final-output))
-    ;(repeatedly #(day7-build-program program)))
-  ;(println "final-output" (deref !final-output))
-  ;(day7-build-program program))
-
-    (deref !final-output))
-
 (defn main-orchestrator [program input]
   (let [a-chan (chan (async/sliding-buffer 2))
-        b-chan (chan (async/sliding-buffer 1))
-        c-chan (chan (async/sliding-buffer 1))
-        d-chan (chan (async/sliding-buffer 1))
-        e-chan (chan (async/sliding-buffer 1))
+        b-chan (chan (async/sliding-buffer 2))
+        c-chan (chan (async/sliding-buffer 2))
+        d-chan (chan (async/sliding-buffer 2))
+        e-chan (chan (async/sliding-buffer 2))
         final-chan (chan (async/sliding-buffer 5))
         !final-out (atom 0)
         [in1 in2 in3 in4 in5] input]
@@ -145,44 +99,42 @@
               (day7-build-program program e-chan a-chan !final-out final-chan))
 
     (loop [count 0]
-      (if (= count 4)
-        (<!! final-chan)
-        (do (<!! final-chan)
-            (recur (inc count)))))))
+      (if (= count 5)
+        (deref !final-out)
+        (do
+          (println "From final: " (<!! final-chan))
+          (recur (inc count)))))))
 
-(comment
-  (dosync (main-orchestrator [3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26
-                              27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5]
-                             [9 8 7 6 5])
+(defn get-all-combinations [start finish]
+  (let [codes (range start finish)]
+    (for [a codes
+          b (remove #{a} codes)
+          c (remove #{a b} codes)
+          d (remove #{a b c} codes)
+          e (remove #{a b c d} codes)]
+      [a b c d e])))
 
-          (main-orchestrator [3 52 1001 52 -5 52 3 53 1 52 56 54 1007 54 5 55 1005 55 26 1001 54
-                              -5 54 1105 1 12 1 53 54 53 1008 54 0 55 1001 55 1 55 2 53 55 53 4
-                              53 1001 56 -1 56 1005 56 6 99 0 0 0 0 10]
-                             [9 7 8 5 6])))
-
-(defn try-all-combinations-part2 [program]
-  (println "HI")
-  (let [codes (range 5 10)
-        code-combos (for [a codes
-                          b (remove #{a} codes)
-                          c (remove #{a b} codes)
-                          d (remove #{a b c} codes)
-                          e (remove #{a b c d} codes)]
-                      [a b c d e])]
-       (reduce (fn [highval combo] 
-                  (max highval (main-orchestrator program combo))) 0 code-combos)
-    #_(map (fn [combo]
-            (println combo)
-            {:combo combo
-             :output _(main-orchestrator program combo)}) code-combos)))
-
-(comment
-  (try-all-combinations-part2 [3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26
-                               27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5]))
+(defn try-all-combinations-part2 [program code-combos]
+  (reduce (fn [highval combo]
+            (println "combo: " combo "highestval: " highval)
+            (max highval (main-orchestrator program combo))) 0 code-combos))
 
 (comment
   #_(apply max-key :output (try-all-combinations root-program))
   ;; => {:combo [3 1 2 0 4], :output 14902} ;; PART 1 Answer
 
+  (try-all-combinations-part2 [3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26
+                               27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5]
+                              (get-all-combinations 5 10))
+  ;; => 141794532
 
+
+  (main-orchestrator root-program [9 8 7 6 5])
+  ;; => 4056746
+
+  (try-all-combinations-part2 root-program (take 60 (get-all-combinations 5 10)))
+  ;; => 6472876
+
+  (try-all-combinations-part2 root-program (drop 60 (get-all-combinations 5 10)))
+  ;; => 6489132
   0)
