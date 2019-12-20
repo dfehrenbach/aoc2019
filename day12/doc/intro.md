d0e8dd087efecb73344d94178e956af251b1fb46
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2019-12-20 11:08:29 -0600

    Stat day 12

---
+++ day12/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to day12
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ day12/test/day12/core_test.clj
@@ -0,0 +1,7 @@
+(ns day12.core-test
+  (:require [clojure.test :refer :all]
+            [day12.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
