5eb62a744570d05115aece236c7a3dbc1a78b78b
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2020-02-03 10:02:49 -0600

    Setup day 18

---
+++ src/day18/core.clj
@@ -0,0 +1,7 @@
+(ns day18.core
+  (:require [clojure.string :as str]
+            [postmortem.instrument :as pi]
+            [postmortem.core :as pm]
+            [postmortem.xforms :as xf]
+            [clojure.math.numeric-tower :as math]
+            [clojure.core.reducers :as reducers]))
---
+++ test/day18/core_test.clj
@@ -0,0 +1,4 @@
+(ns day18.core-test
+  (:require [day18.core :as sut]
+            [clojure.test :as t]))
+
